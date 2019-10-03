3d8a48ca9922a831a6be1df2049aa933778a1f05
AugustoQueiroz <asq@cin.ufpe.br>
2019-10-03 17:08:50 +0200

    Added command-line argument management

---
+++ arg_mng.py
@@ -0,0 +1,14 @@
+import argparse
+
+def createParser():
+    parser = argparse.ArgumentParser(description="Simulate the movement of people in a room")
+    parser.add_argument("-p", type=int, help="An int in the range [0,9] that codes the number of people in the terrain\nThe number of people = 2^p", required=True)
+    parser.add_argument("-t", type=int, help="Mode in which the program should be run.\n0 - Single Thread,\n1 - One thread for each person,\n2 - One thread for each quadrant of the terrain", required=True)
+    parser.add_argument("-m", action="store_true")
+
+    return parser
+
+if __name__ == "__main__":
+    parser = createParser()
+    args = parser.parse_args()
+    print(args)

27bcd5ef8d0198ea0af93647208db5331c8646ed
AugustoQueiroz <asq@cin.ufpe.br>
2019-10-20 22:26:49 +0200

    Added some outputs to know where the code is

--- main.py
+++ main.py
@@ -5,11 +5,17 @@ from src.Terrain import Terrain
 parser = arg_mng.createParser()
 args = parser.parse_args()
 
+print("Generating Terrain")
 terrain = Terrain.RandomTerrain(n_people=2**args.p)
+print("Terrain Generated")
+print("Instantiating Simulator")
 simulator = MobSimulator(terrain)
+print("Simulator Instantiated")
 
 if args.t == 0:
+    print("Running Simulation")
     simulator.run_single_threaded()
+    print("Simulation Finished")
 elif args.t == 1:
     simulator.run_one_thread_per_person()
 elif args.t == 2:
